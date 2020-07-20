a1fb7d5e5322775e8a91adfd245264125e10558a
jongwhan kim <jjjong50@naver.com>
2020-07-20 12:23:19 +0900

    First Commit Flask_Web

---
+++ app.py
@@ -0,0 +1,20 @@
+# Flask - server 띄우기
+# render_template - template render 해줌 - html 문서를 html 형식으로 변환해서 날려줌
+from flask import Flask , render_template
+
+app = Flask(__name__)
+app.debug=True
+
+# @ : 데코레이션
+# route : 중계 - 서버의 경로 요청에 따라 무엇을 할지 결정 - 경로 지정
+@app.route('/data')
+def index():
+    print("Success")
+    # return "TEST"
+    return render_template('home.html')
+
+
+#local host ip address 127.0.0.1
+if __name__ == '__main__' :
+    # app.run(host = '0.0.0.0', port='6000')
+    app.run()
