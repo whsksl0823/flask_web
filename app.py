a7ed4c2a0ed4e436b58954627e7dbd806760662a
jongwhan kim <jjjong50@naver.com>
2020-07-23 17:48:06 +0900

    add_articles delete edit step 2

--- app.py
+++ app.py
@@ -97,13 +97,17 @@ def articles():
     # return "GET Success"
     return render_template('articles.html',articles=data)
 
-@app.route('/article/<int:id>')
+@app.route('/article/<string:id>')
 def article(id):
-    print(id)
-    articles= Articles()[id-1]
-    # print(articles)
-    return render_template('article.html',data =articles)
-    # return "Success"
+    # print(id)
+    # articles= Articles()[id-1]
+
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic WHERE id=%s;'
+    cursor.execute(sql, [id])
+    topic = cursor.fetchone()
+    print(topic)
+    return render_template('article.html',data =topic)
 
 @app.route('/add_articles', methods=['GET', 'POST'])
 def add_articles():
@@ -125,7 +129,39 @@ def add_articles():
         return render_template('add_articles.html')
         db.close()
 
+@app.route('/article/<string:id>/edit_article', methods=['GET', 'POST'])
+def edit_article(id):
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        
+        cur = db.cursor()
+        sql = '''
+            UPDATE `topic` SET `title`=%s, `body`=%s, `author`=%s WHERE  `id`=%s;
+        '''
+        cur.execute(sql, (title, body, author, [id]))
+        db.commit()
+        return redirect(url_for('articles'))
+    else:
+        print(id)
+        cur = db.cursor()
+        sql = 'SELECT * FROM topic WHERE id=%s'
+        cur.execute(sql, [id])
+        topic = cur.fetchone()
+        print(topic)
 
+        return render_template('edit_article.html', data = topic)
+        db.close()
+@app.route('/delete/<string:id>', methods=['POST'])
+def delete(id):
+    cursor = db.cursor()
+    sql = 'DELETE FROM `topic` WHERE  `id`=%s;'
+    cursor.execute(sql, [id])
+    db.commit()
+    
+    return redirect(url_for('articles'))
+    db.close()
 
 if __name__ =='__main__':
     # app.run(host='0.0.0.0', port='8080')
